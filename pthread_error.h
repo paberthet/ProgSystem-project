ee80171942de1fc8266b45de78090033a17f04c7
Pierre-Augustin BERTHET <68542085+paberthet@users.noreply.github.com>
2021-05-22 14:49:38 +0200

    Update pthread_error.h

--- pthread_error.h
+++ pthread_error.h
@@ -1,6 +1,11 @@
 #ifndef PTHREADERR
 #define PTHREADERR
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
 extern void thread_error(const char* file, int line, int code, char* txt);
 
 #endif
