800dc176e98a599b6f82ed51e6a214773b940355
Pierre-Augustin BERTHET <68542085+paberthet@users.noreply.github.com>
2021-05-25 10:20:38 +0200

    added prefix_slash

--- init_cond.c
+++ init_cond.c
@@ -1,4 +1,5 @@
 #include "init_cond.h"
+#define L_NAME 256
 
 /* This file is dedicated to initializing the condition variable. It ll be used by the server and the clients to interact between them */
 
@@ -12,4 +13,23 @@ int initialiser_cond(pthread_cond_t * pcond)
     return code;
   code = pthread_cond_init(pcond, &condattr);
   return code;
+}
+
+/*prefix slash allows to get the path of an object*/
+
+char *prefix_slash(const char* name)
+{
+  char *nom = malloc(L_NAME);
+  if(name[1] != '/')
+  {
+    nom[0] = '/';
+    nom[1] = '\0';
+    strncat(nom,name,L_NAME+3);
+  }
+  else
+  {
+    strncpy(nom,name,L_NAME);
+    nom[L_NAME-1] = '\0';
+  }
+  return nom;
 }
