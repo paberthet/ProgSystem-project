f80c542fe0b4a17dbab2a6716045e2fa3947d05b
Pierre-Augustin BERTHET <68542085+paberthet@users.noreply.github.com>
2021-05-25 09:35:59 +0200

    Create panic.h

---
+++ panic.h
@@ -0,0 +1,22 @@
+/*code provided by our teacher, W. Zielonka */
+
+#ifndef PANIC_H
+#define PANIC_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#define PANIC_EXIT( msg )  do{			\
+   fprintf(stderr,\
+     "\n %d %s : error \"%s\" in file %s in line %d\n",\
+	   (int) getpid(), msg, strerror(errno), __FILE__, __LINE__);	\
+   exit(1);\
+  } while(0)		
+#define PANIC( msg )  do{			\
+   fprintf(stderr,\
+     "\n %d %s : error \"%s\" in file %s in line %d\n",\
+	   (int) getpid(),msg, strerror(errno), __FILE__, __LINE__);	 \
+  } while(0)		
+#endif
