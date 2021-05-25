2fdb23f2a2675197cb1ae322a3842b4eee3ed044
Pierre-Augustin BERTHET <68542085+paberthet@users.noreply.github.com>
2021-05-25 09:42:58 +0200

    added memory object here

--- init_cond.h
+++ init_cond.h
@@ -5,4 +5,18 @@
 
 int initialiser_cond(pthread_cond_t * pcond);
 
+/*====================================================
+The memory header
+====================================================*/
+
+typedef struct{
+  pthread_mutex_t mutex;
+  pthread_cond_t ccond; //client cond
+  pthread_cond_t scond; //server cond
+  pid_t pid;
+  int erreur;
+  int libre;
+  int retourFonction; // 0 if everything s alright, -1 if not
+}memory; //Data are stored just after the header, as we overmap the shm for this purpose
+
 #endif
